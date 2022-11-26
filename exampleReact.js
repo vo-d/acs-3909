3e9375bb05a66c30580e1dd3e82f614fd6991075
vo-d <daidaivo2002@gmail.com>
2022-11-25 23:53:41 -0600

    react

--- exampleReact.js
+++ exampleReact.js
@@ -129,3 +129,34 @@ class ShoppingCart1 extends React.Component {
 ShoppingCart.propTypes ={
     items: PropTypes.number.isRequired
 }
+
+//Access Props Using this.props
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+          <div>
+              { /* Change code below this line */ }
+              <Welcome name ={"Dai"}/>
+              { /* Change code above this line */ }
+          </div>
+      );
+    }
+};
+  
+class Welcome extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+          <div>
+            { /* Change code below this line */ }
+            <p>Hello, <strong>{this.props.name}</strong>!</p>
+            { /* Change code above this line */ }
+          </div>
+      );
+    }
+};
